a950238563306dd743c6baa6fbb6e73c4c436f5e
mhittle <mhittle@gmail.com>
2020-05-10 16:23:10 -0700

    remove category

--- R/fb_campaign.R
+++ R/fb_campaign.R
@@ -7,7 +7,6 @@
 #' @param objective the campaign's objective
 #' @param spend_cap spend cap of the campaign
 #' @return Ad Campaign id
-#' @param special_ad_category special ad category
 #' @param special_ad_categories special ad categories
 #' @export
 #' @references \url{https://developers.facebook.com/docs/marketing-api/reference/ad-campaign-group#Creating}
@@ -22,7 +21,7 @@ fbad_create_campaign <- function(fbacc, buying_type = c('AUCTION', 'FIXED_CPM', 
                                      'MOBILE_APP_ENGAGEMENT', 'MOBILE_APP_INSTALLS',
                                      'OFFER_CLAIMS', 'PAGE_LIKES', 'POST_ENGAGEMENT',
                                      'PRODUCT_CATALOG_SALES', 'VIDEO_VIEWS'),
-                                 spend_cap = NULL, special_ad_category,special_ad_categories) {
+                                 spend_cap = NULL,special_ad_categories) {
 
     fbacc <- fbad_check_fbacc()
     if (missing(name))
@@ -39,7 +38,6 @@ fbad_create_campaign <- function(fbacc, buying_type = c('AUCTION', 'FIXED_CPM', 
         spend_cap         = spend_cap,
         objective         = match.arg(objective),
         campaign_status   = match.arg(campaign_status),
-        special_ad_category = special_ad_category,
         special_ad_categories = special_ad_categories)
 
     ## drop NULL args
